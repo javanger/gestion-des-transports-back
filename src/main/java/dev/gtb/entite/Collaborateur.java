7c3eca7b25dcb037dd964a2888f62a74f45b7d72
Guillaume <guillaume.gobert@gmail.com>
2018-04-30 14:43:05 +0200

    correction entite collaborateur, chauffeur et employé

--- src/main/java/dev/gtb/entite/Collaborateur.java
+++ src/main/java/dev/gtb/entite/Collaborateur.java
@@ -3,6 +3,7 @@
  */
 package dev.gtb.entite;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 
 /**
@@ -12,8 +13,12 @@ import javax.persistence.Entity;
 @Entity
 public class Collaborateur extends Employe {
 
-	public Collaborateur(String nom, String prenom, String email, String telephone, String urlPhoto) {
-		super(nom, prenom, email, telephone, urlPhoto);
+	@Column(name = "ADMINISTRATEUR")
+	private Boolean administrateur;
+
+	public Collaborateur(String nom, String prenom, String email,String motDePasse, String telephone, String urlPhoto, Boolean administrateur) {
+		super(nom, prenom, email, motDePasse, telephone, urlPhoto);
+		this.administrateur = administrateur;
 	}
 
 }
