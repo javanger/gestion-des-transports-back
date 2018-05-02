7ac0021242d1bf6fae194f705755d67ded999d02
Valentin <lefebvre.valentin76@gmail.com>
2018-05-02 10:44:48 +0200

    Ajout du jeu de données

--- src/main/java/dev/entite/Chauffeur.java
+++ src/main/java/dev/entite/Chauffeur.java
@@ -15,10 +15,17 @@ public class Chauffeur extends Employe {
 
 	@Column(name = "PERMIS")
 	private Boolean permis;
+	@Column(name = "TELEPHONE")
+	private String telephone;
 
 	
-	public Chauffeur(String nom, String prenom, String email,String motDePasse, String telephone, String urlPhoto, Boolean permis) {
-		super(nom, prenom, email, motDePasse, telephone, urlPhoto);
+	public Chauffeur() {
+		super();
+	}
+
+	public Chauffeur(String matricule, String nom, String prenom, String email, String motDePasse, String urlPhoto,
+			Boolean permis) {
+		super(matricule, nom, prenom, email, motDePasse, urlPhoto);
 		this.permis = permis;
 	}
 
@@ -41,4 +48,23 @@ public class Chauffeur extends Employe {
 		this.permis = permis;
 	}
 	
+	/**
+	 * Getter
+	 * 
+	 * @return the telephone
+	 */
+	public String getTelephone() {
+		return telephone;
+	}
+
+	/**
+	 * Setter
+	 * 
+	 * @param telephone
+	 *            the telephone to set
+	 */
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+
 }
--- src/main/java/dev/service/InitialiserDonnesServiceDev.java
+++ src/main/java/dev/service/InitialiserDonnesServiceDev.java
@@ -1,52 +1,35 @@
 package dev.service;
 
+import java.util.stream.Stream;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Service;
 
-import dev.entite.CategorieVehicule;
+import dev.entite.AnnonceCovoiturage;
+import dev.entite.Chauffeur;
+import dev.entite.Collaborateur;
+import dev.entite.Reservation;
 import dev.entite.Vehicule;
-import dev.repository.VehiculeRepository;
 
 @Service
 public class InitialiserDonnesServiceDev implements InitialiserDonneesService {
 	@PersistenceContext
 	private EntityManager em;
 
-	@Autowired
-	private VehiculeRepository vehiculeRepo;
 
 	@Override
 	@Transactional
 	public void initialiser() {
-		// TODO Auto-generated method stub
-		Vehicule voiture1 = new Vehicule("22-ABCD-44", "Toyota", "yaris", CategorieVehicule.COMPACTE, 5,
-				"http://www.envoiturecarine.fr/wp-content/uploads/2015/08/lamborghini-aventador.jpg");
-		Vehicule voiture2 = new Vehicule("54-ABCD-63", "Renault", "megane", CategorieVehicule.CITADINE_POLYVALENTE, 5,
-				"http://www.envoiturecarine.fr/wp-content/uploads/2015/08/lamborghini-aventador.jpg");
-		Vehicule voiture3 = new Vehicule("23-ABCD-47", "Peugeot", "308", CategorieVehicule.MINI_CITADINE, 2,
-				"http://www.envoiturecarine.fr/wp-content/uploads/2015/08/lamborghini-aventador.jpg");
-		Vehicule voiture4 = new Vehicule("69-ABCD-69", "Dacia", "duster", CategorieVehicule.TOUT_TERRAINS, 5,
-				"http://www.envoiturecarine.fr/wp-content/uploads/2015/08/lamborghini-aventador.jpg");
-		Vehicule voiture5 = new Vehicule("62-ABCD-44", "Volvo", "xc350", CategorieVehicule.valueOf("COMPACTE"), 5,
-				"http://www.envoiturecarine.fr/wp-content/uploads/2015/08/lamborghini-aventador.jpg");
-		Vehicule voiture6 = new Vehicule("59-ABCD-87", "huyndai", "i20", CategorieVehicule.valueOf("COMPACTE"), 2,
-				"http://www.envoiturecarine.fr/wp-content/uploads/2015/08/lamborghini-aventador.jpg");
 
-		vehiculeRepo.save(voiture1);
-		vehiculeRepo.save(voiture2);
-		vehiculeRepo.save(voiture3);
-		vehiculeRepo.save(voiture4);
-		vehiculeRepo.save(voiture5);
-		vehiculeRepo.save(voiture6);
-		System.out.println(voiture6);
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("configWeb.xml");
 
+		Stream.of(Collaborateur.class, Chauffeur.class, AnnonceCovoiturage.class, Reservation.class, Vehicule.class)
+				.flatMap(classe -> context.getBeansOfType(classe).values().stream()).forEach(em::persist);
+		context.close();
 
-
-
-
-}
+	}
 }
