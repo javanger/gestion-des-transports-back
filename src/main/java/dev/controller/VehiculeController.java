c23abf1342d742d4d491d708e94e94c1db2c84ec
victorahs <victorahsene90@gmail.com>
2018-05-02 16:17:31 +0200

    creation ajouter une voiture

--- src/main/java/dev/controller/VehiculeController.java
+++ src/main/java/dev/controller/VehiculeController.java
@@ -5,14 +5,16 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import dev.entite.Vehicule;
 import dev.repository.VehiculeRepository;
 
-@CrossOrigin
 @RestController
+@CrossOrigin
 @RequestMapping("/vehicules")
 public class VehiculeController {
 
@@ -22,6 +24,21 @@ public class VehiculeController {
 	@GetMapping
 	public List<Vehicule> listerVehicules() {
 		return this.vehiculeRepository.findAll();
+	}
+
+	@PostMapping("/ajouter")
+	public Vehicule ajoutVehicule(@RequestBody Vehicule vehicule) {
+		Vehicule v = new Vehicule();
+		v.setImmatriculation(vehicule.getImmatriculation());
+		v.setMarque(vehicule.getMarque());
+		v.setModele(vehicule.getModele());
+		v.setNbPlaces(vehicule.getNbPlaces());
+		v.setCategorie(vehicule.getCategorie());
+		v.setUrlPhoto(vehicule.getUrlPhoto());
+		vehiculeRepository.save(v);
+
+		return v;
+
 	}
 
 }
