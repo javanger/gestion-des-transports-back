b1ef541293b5f4c5ef081c7d67ffc6b74460091e
Guillaume <guillaume.gobert@gmail.com>
2018-05-17 14:21:55 +0200

    correction back

--- src/main/java/dev/DemoApplication.java
+++ src/main/java/dev/DemoApplication.java
@@ -4,9 +4,25 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class DemoApplication {
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*")
+						.allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+						.allowedHeaders("Content-Type", "Access-Control-Allow-Origin", "Access-Control-Allow-Headers",
+								"Authorization", "X-Requested-With", "requestId", "Correlation-Id", "withCredentials");
+			}
+		};
+	}
 
 	@Bean
 	public ModelMapper modelMapper() {
--- src/main/java/dev/config/WebSecurityConfig.java
+++ src/main/java/dev/config/WebSecurityConfig.java
@@ -6,16 +6,13 @@ package dev.config;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 /**
  * @author Axel B.
@@ -24,34 +21,22 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(securedEnabled = false)
+@CrossOrigin
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	private DataSource dataSource;
 
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurerAdapter() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("*")
-						.allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
-						.allowedHeaders("Content-Type", "Access-Control-Allow-Origin", "Access-Control-Allow-Headers",
-								"Authorization", "X-Requested-With", "requestId", "Correlation-Id", "withCredentials");
-			}
-		};
-	}
-
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests()
-				// .antMatchers("/collaborateur/**")
+		http.authorizeRequests() // .antMatchers("/collaborateur/**")
 				// .access("hasRole('ROLE_COLLABORATEUR') and
 				// hasRole('ROLE_ADMIN')").antMatchers("/admin/**")
 				// .hasRole("ADMINISTRATEUR").antMatchers("/chauffeur/**")
-				// .access("hasRole('ROLE_CHAUFFEUR') and
+				// .access("hasRole('ROLE_CHAUFFEUR') and //
 				// hasRole('ROLE_ADMIN')")
 				.anyRequest().permitAll();
+		http.csrf().disable();
 
 	}
 
